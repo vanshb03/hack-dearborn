25f887b734819084b3fa6e3b6e74a85a79072510
Vansh <bataviyavansh@gmail.com>
2024-10-13 03:18:01 -0400

    rem: unused file

--- lib/getAccessToken.ts
+++
@@ -1,7 +0,0 @@
-import { supabase } from "@/supabaseClient";
-
-export const getAccessToken = async () => {
-  const session = await supabase.auth.getSession();
-  const accessToken = session?.data.session?.provider_token; // Retrieve access token from session
-  return accessToken;
-};
